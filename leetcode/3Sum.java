004d821a514adddcb05198e2edcf2abad9d23350
junwi <perfly@hotmail.com>
2018-10-16 12:44:50 +0800

    Update 3Sum.java

--- leetcode/3Sum.java
+++ leetcode/3Sum.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Solution {
     
-    public ArrayList<ArrayList<Integer>> threeaSum(int[] num) {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         Arrays.sort(num);
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         for (int i = 0; i < num.length - 2; ++i) {
