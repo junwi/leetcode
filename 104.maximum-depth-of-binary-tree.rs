4e4cd87f4b9e74fc306fb6a63bff6ec4e66add58
wangwei07 <weiwang07@kuaishou.com>
2023-02-16 17:47:52 +0800

    104 solution

---
+++ 104.maximum-depth-of-binary-tree.rs
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=104 lang=rust
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                cmp::max(
+                    Self::max_depth(node.borrow().left.clone()),
+                    Self::max_depth(node.borrow().right.clone())
+                ) + 1
+            }
+            None => 0
+        }
+    }
+}
+// @lc code=end
+
