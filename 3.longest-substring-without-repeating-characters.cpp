8a55289b6cbe5cceb41f662116407a3620be4f3f
wangwei07 <weiwang07@kuaishou.com>
2023-02-20 15:43:33 +0800

    3 solution optimize

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -8,14 +8,15 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> map;
+        int map[256] = {0};
         int l = 0;
+        int n = s.size();
         int result = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (map.count(s[i]) > 0 && map[s[i]] >= l) {
-                l = map[s[i]] + 1;
+        for (int i = 0; i < n; i++) {
+            if (map[s[i]] > 0 && map[s[i]] >= l) {
+                l = map[s[i]];
             }
-            map[s[i]] = i;
+            map[s[i]] = i + 1;
             result = max(result, i - l + 1);
         }
 
