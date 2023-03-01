56a19ba95e27d1dd2e54bbc8bc291ecf523a653c
wangwei07 <weiwang07@kuaishou.com>
2023-03-01 11:41:06 +0800

    450 solution

--- 450.delete-node-in-a-bst.rs
+++ 450.delete-node-in-a-bst.rs
@@ -25,62 +25,56 @@
 // }
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::ops::Deref;
 impl Solution {
     pub fn delete_node(root: Option<Rc<RefCell<TreeNode>>>, key: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        let mut current: Rc<RefCell<TreeNode>> = root.to_owned().unwrap();
-        let mut parent: &Option<Rc<RefCell<TreeNode>>>;
-        while let Some(node) = current {
-            if node.borrow().val == key {
-                break;
-            }
-            parent = current;
-            if node.borrow().val > key {
-                current = &node.borrow().deref().left;
-            } else {
-                current = &node.borrow().right;
-            }
-        }
-        if current.is_none() {
+        if root.is_none() {
             return root;
         }
-        if let Some(node) = current {
-            if node.borrow().left.is_some() && node.borrow().right.is_some() {
-                let mut tmp = node;
-                parent = current;
-                current = &node.borrow().right;
-                while let Some(n) = current {
-                    if n.borrow().left.is_some() {
-                        parent = current;
-                        current = &n.borrow().left;
-                    } else {
-                        tmp.borrow().val = n.borrow().val;
-                        break;
-                    }
-                }
+        let mut current: Rc<RefCell<TreeNode>> = root.clone().unwrap();
+        let mut parent: Rc<RefCell<TreeNode>> = current.clone();
+        loop {
+            if key == current.borrow().val {
+                break;
+            }
+            parent = current.clone();
+            if key < current.borrow().val && current.borrow().left.is_some() {
+                current = current.clone().borrow().left.clone().unwrap();
+            } else if key > current.borrow().val && current.borrow().right.is_some() {
+                current = current.clone().borrow().right.clone().unwrap();
+            } else {
+                break;
             }
         }
-        if let Some(node) = current {
-            if parent.is_none() {
-                if node.borrow().left.is_some() {
-                    return node.borrow().left;
-                } else {
-                    return node.borrow().right;
-                }
+        if key != current.borrow().val {
+            return root;
+        }
+        if current.borrow().left.is_some() && current.borrow().right.is_some() {
+            let tmp = current.clone();
+            parent = current.clone();
+            current = current.clone().borrow().right.clone().unwrap();
+            while current.borrow().left.is_some() {
+                parent = current.clone();
+                current = current.clone().borrow().left.clone().unwrap();
             }
-            let mut tmp: Option<Rc<RefCell<TreeNode>>> = None;
-            if node.borrow().left.is_some() {
-                tmp = node.borrow().left;
+            tmp.borrow_mut().val = current.borrow().val;
+        }
+        if current == root.clone().unwrap() {
+            if current.borrow().left.is_some() {
+                return current.clone().borrow().left.clone();
             } else {
-                tmp = node.borrow().right;
+                return current.clone().borrow().right.clone();
             }
-            if let Some(p) = parent {
-                if p.borrow().left == *current {
-                    p.borrow().left = tmp;
-                } else {
-                    p.borrow().right = tmp;
-                }
-            }
+        }
+        let mut tmp: Option<Rc<RefCell<TreeNode>>> = None;
+        if current.borrow().left.is_some() {
+            tmp = current.clone().borrow().left.clone();
+        } else {
+            tmp = current.clone().borrow().right.clone();
+        }
+        if parent.borrow().left == Some(current) {
+            parent.borrow_mut().left = tmp;
+        } else {
+            parent.borrow_mut().right = tmp;
         }
 
         return root;
