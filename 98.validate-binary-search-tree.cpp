91c9c576af2fe73c61cc639dfb572aec95ea27d3
wangwei07 <weiwang07@kuaishou.com>
2023-03-02 16:28:41 +0800

    98 solution

---
+++ 98.validate-binary-search-tree.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        TreeNode* prev = nullptr;
+        while (node != nullptr) {
+            while (node->left != nullptr) {
+                st.push(node);
+                node = node->left;
+            }
+            while (node->right == nullptr && !st.empty()) {
+                if (prev != nullptr && prev->val >= node->val) {
+                    return false;
+                }
+                prev = node;
+                node = st.top();
+                st.pop();
+            }
+            if (prev != nullptr && prev->val >= node->val) {
+                return false;
+            }
+            prev = node;
+            node = node->right;
+        }
+
+        return true;
+    }
+};
+// @lc code=end
+
