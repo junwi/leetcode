5ea02005521b2c5e9f1467b7fca6c9dd3bca9231
wangwei07 <weiwang07@kuaishou.com>
2023-03-07 17:58:49 +0800

    51 solution

--- 51.n-queens.java
+++ 51.n-queens.java
@@ -1,4 +1,7 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
 
 /*
@@ -9,53 +12,55 @@ import java.util.List;
 
 // @lc code=start
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        boolean[][] board = new boolean[n + 1][n + 1];
-        List<List<String>> result = new ArrayList<>();
+    private boolean[] col;
+    private boolean[] main;
+    private boolean[] sub;
+    private int[] path;
 
-        solve(board, result, 1);
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        if (n == 0) {
+            return result;
+        }
+
+        col = new boolean[n];
+        main = new boolean[2 * n];
+        sub = new boolean[2 * n];
+        path = new int[n];
+
+        solve(result, 0, n);
 
         return result;
     }
 
-    private void solve(boolean[][] board, List<List<String>> result, int count) {
-        int n = board.length - 1;
-        if (count == n + 1) {
-            result.add(stringify(board));
+    private void solve(List<List<String>> result, int x, int n) {
+        if (x == n) {
+            result.add(stringify());
             return;
         }
-        for (int i = 1; i <= n; i++) {
-            board[count][i] = true;
-            if (validate(board, count, i)) {
-                solve(board, result, count + 1);
+        for (int y = 0; y < n; y++) {
+            if (col[y] || main[x - y + n] || sub[x + y]) {
+                continue;
             }
-            board[count][i] = false;
+            col[y] = true;
+            main[x - y + n] = true;
+            sub[x + y] = true;
+            path[x] = y;
+            solve(result, x + 1, n);
+            col[y] = false;
+            main[x - y + n] = false;
+            sub[x + y] = false;
         }
     }
 
-    private boolean validate(boolean[][] board, int x, int y) {
-        int n = board.length - 1;
-        for (int i = 1; i < x; i++) {
-            if (i < x && board[i][y]) return false;
-        }
-        for (int i = x - 1, j = y - 1; i > 0 && j > 0; i--, j--) {
-            if (board[i][j]) return false;
-        }
-        for (int i = x - 1, j = y + 1; i > 0 && j <= n; i--, j++) {
-            if (board[i][j]) return false;
-        }
-
-        return true;
-    }
-
-    private List<String> stringify(boolean[][] board) {
+    private List<String> stringify() {
         List<String> result = new ArrayList<>();
-        int n = board.length - 1;
-        for (int i = 1; i <= n; i++) {
+        int n = path.length;
+        for (int i : path) {
             StringBuilder sb = new StringBuilder();
-            for (int j = 1; j <= n; j++) {
-                sb.append(board[i][j] ? 'Q' : '.');
-            }
+            sb.append(".".repeat(n));
+            sb.replace(i, i + 1, "Q");
+
             result.add(sb.toString());
         }
 
