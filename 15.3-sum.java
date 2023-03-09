dc4ec6c0da8471bfc17a9a403d978c636ef1ded7
wangwei07 <weiwang07@kuaishou.com>
2023-03-09 17:54:56 +0800

    15 solution

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -13,17 +13,18 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; ++i) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1, k = nums.length - 1, target = -nums[i];
+            int j = i + 1, k = nums.length - 1;
             while (j < k) {
-                int sum = nums[j] + nums[k];
-                if (sum == target) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     ++j;
                     --k;
                     while (j < k && nums[j] == nums[j - 1]) ++j;
                     while (j < k && nums[k] == nums[k + 1]) --k;
-                } else if (sum > target) {
+                } else if (sum > 0) {
                     --k;
                 } else {
                     ++j;
