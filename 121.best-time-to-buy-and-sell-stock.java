d173e074f956e6666373f943bfc57e8cd168c0a6
wangwei07 <wangwei07@kuaishou.com>
2023-05-23 21:07:36 +0800

    121.best-time-to-buy-and-sell-stock.java

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -7,18 +7,13 @@
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
-        int have = -prices[0];
+        int buy = -prices[0], sell = 0;
         for (int i = 1; i < prices.length; i++) {
-            int gap = prices[i] + have;
-            if (gap > max) {
-                max = gap;
-            } else {
-                have = Math.max(have, -prices[i]);
-            }
+            buy = Integer.max(buy, -prices[i]);
+            sell = Integer.max(sell, buy + prices[i]);
         }
 
-        return max;
+        return sell;
     }
 }
 // @lc code=end
