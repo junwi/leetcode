055296211099864fcf8ce5f670da27be112ca5cf
wangwei07 <wangwei07@kuaishou.com>
2023-06-13 11:33:40 +0800

    142.linked-list-cycle-ii.java

---
+++ 142.linked-list-cycle-ii.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
+    }
+}
+// @lc code=end
+
